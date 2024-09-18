2c3e60c14272d7ba6a7e46203d8d91a052dc3dd4
matroxok <matrox.biznesyt@gmail.com>
2024-09-18 21:51:20 +0200

    finish project (version 1.0.1)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
 let questions = document.querySelectorAll('.question-box')
-let answers = document.querySelectorAll('.answer-box')
 
-const test = () => {}
+// Oryginal Icon (plus icon)
+const originalIconPath =
+	'M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z'
+const originalIconFill = '#AD28EB'
 
-questions.forEach(click => click.addEventListener('click', test))
+// Icon after click (minus icon)
+const minusIconPath =
+	'M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z'
+const minusIconFill = '#301534'
+
+questions.forEach(question => {
+	question.addEventListener('click', () => {
+		let answer = question.nextElementSibling
+
+		// Locate path and icon in div "queston-box"
+		const pathElement = question.querySelector('.icon path')
+
+		if (answer.style.display === 'block') {
+			// hide answer (setting atribute display to "none") and change icon to default
+			answer.style.display = 'none'
+			question.classList.remove('clicked')
+			if (pathElement) {
+				updateSVGPatch(pathElement, originalIconPath, originalIconFill)
+			}
+		} else {
+			// Showing answer and changing icon
+			question.classList.add('clicked')
+			answer.style.display = 'block'
+			if (pathElement) {
+				updateSVGPatch(pathElement, minusIconPath, minusIconFill)
+			}
+		}
+	})
+})
+
+// upadte path, color
+const updateSVGPatch = (pathElement, newPath, newFill) => {
+	pathElement.setAttribute('d', newPath)
+	pathElement.setAttribute('fill', newFill)
+}
